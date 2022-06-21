cad0e7d74bb2e8df7b4ebb8996d676c04d08651d
koustov <koustov@live.com>
2022-06-21 11:10:33 +0530

    argument parser

--- bin/global.js
+++ bin/global.js
@@ -1,6 +1,32 @@
 #!/usr/bin/env node
 
-import { log } from "../lib/index.js";
+import { log, invoke_process } from "../lib/index.js";
+import { CONSOLE_COLORS } from "../util/const.js";
 
 // Displays the text in the console
-log("This is react-play development kit");
+
+console.log(
+  CONSOLE_COLORS.FgGreen,
+  "┌─────────────────────────────────────────────────────────────────────────┐"
+);
+console.log(
+  " |                                                                         |"
+);
+console.log(
+  " |                               Welcome to                                |"
+);
+console.log(
+  " |                       React Play Development Kit                        |"
+);
+console.log(
+  " |                                                                         |"
+);
+console.log(
+  " |                       For help hit : rpdk -h/help                       |"
+);
+console.log(
+  " └─────────────────────────────────────────────────────────────────────────┘"
+);
+
+console.log(CONSOLE_COLORS.Reset, "");
+invoke_process();
---
+++ util/const.js
@@ -0,0 +1,54 @@
+export const CONSOLE_COLORS = {
+  Reset: "\x1b[0m",
+  Bright: "\x1b[1m",
+  Dim: "\x1b[2m",
+  Underscore: "\x1b[4m",
+  Blink: "\x1b[5m",
+  Reverse: "\x1b[7m",
+  Hidden: "\x1b[8m",
+
+  FgBlack: "\x1b[30m",
+  FgRed: "\x1b[31m",
+  FgGreen: "\x1b[32m",
+  FgYellow: "\x1b[33m",
+  FgBlue: "\x1b[34m",
+  FgMagenta: "\x1b[35m",
+  FgCyan: "\x1b[36m",
+  FgWhite: "\x1b[37m",
+
+  BgBlack: "\x1b[40m",
+  BgRed: "\x1b[41m",
+  BgGreen: "\x1b[42m",
+  BgYellow: "\x1b[43m",
+  BgBlue: "\x1b[44m",
+  BgMagenta: "\x1b[45m",
+  BgCyan: "\x1b[46m",
+  BgWhite: "\x1b[47m",
+};
+
+export const ALL_ARGS_TYPES = [
+  {
+    name: "create",
+    args: ["create", "-c"],
+    description: "Create a play",
+    flag: false,
+  },
+  {
+    name: "update",
+    args: ["update", "-u"],
+    description: "Update a play",
+    flag: false,
+  },
+  {
+    name: "push",
+    args: ["push", "-p"],
+    description: "Push changes to your fork branch",
+    flag: false,
+  },
+  {
+    name: "help",
+    args: ["help", "-h"],
+    description: "Print all available commands",
+    flag: true,
+  },
+];
