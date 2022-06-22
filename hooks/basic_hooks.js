4f4d146488e6502e8535e1bebc8ff782d8106ba1
koustov <koustov@live.com>
2022-06-22 20:34:07 +0530

    temporary code to show shell execution

--- hooks/basic_hooks.js
+++ hooks/basic_hooks.js
@@ -1,4 +1,21 @@
+import { exec } from "child_process";
+
 export const basic_get_package_manager = () => {
   console.log("Checking package manager");
   return "yarn";
+};
+
+export const basic_check_shell_connectivity = () => {
+  console.log("Checking shell connectivity");
+  exec("sh ./scripts/sample.sh", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
 };
